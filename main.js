0c32450746de86bc589030286b60e0b78aa222ec
Emmanuel Tar <emmanueltar14@gmail.com>
2024-05-24 08:08:54 +0100

    Update: Creating a Strike through in the List

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-const ITEMS_CONTAINER=document.getElementById("items")
-const ITEMS_TEMPLATE=document.getElementById("itemTemplate")
-const ADD_BUTTON=document.getElementById("add")
+const ITEMS_CONTAINER=document.getElementById("items");
+const ITEMS_TEMPLATE=document.getElementById("itemTemplate");
+const ADD_BUTTON=document.getElementById("add");
 
 let items = getItems();
 
 function getItems(){
-const value = localStorage.getItem("todo-test") || "[]";
+    const value = localStorage.getItem("todo-test") || "[]";
 
 return JSON.parse(value);
 
 }
+function setItems(items){
+    const itemsJson = JSON.stringify(items);
 
-getItem(items);
+    localStorage.setItem("todo-test",itemsJson);
+}
+
+function addItem(){
+    items.unshift({
+        description: " ",;
+        completed:false;
+    });
+
+    setItems(items);
+    refreshList();
+}
+
+function refreshList(){
+    //TODO sort items
+    ITEMS_CONTAINER.innerHTML = "";
+
+    for(const item of items){
+        const itemElement = ITEMS_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        ITEMS_CONTAINER>append(itemElement);
+    }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+});
+
+refreshList();
